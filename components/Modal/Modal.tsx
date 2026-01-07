e69136b37babc3fe45f4e631558e7e9a1c2928f8
Rostislav <operkaka4@gmail.com>
2026-01-07 22:33:22 +0300

    finish

---
+++ components/Modal/Modal.tsx
@@ -0,0 +1,39 @@
+import css from "./Modal.module.css";
+import  NoteForm  from "@/components/NoteForm/NoteForm";
+import { createPortal } from "react-dom";
+import { useEffect } from "react";
+
+interface ModalProp {
+    closeModal: () => void
+}
+
+ 
+
+export default function Modal({closeModal}:ModalProp) {
+
+const handleBackdropClick = (event: React.MouseEvent<HTMLDivElement>) => {
+    if (event.target === event.currentTarget) {
+      closeModal();
+    }
+}
+
+ useEffect(() => {
+	  const handleKeyDown = (e: KeyboardEvent) => {
+	    if (e.key === "Escape") {
+	      closeModal();
+	    }
+	  };
+	
+	  document.addEventListener("keydown", handleKeyDown);
+	
+	  return () => {
+	    document.removeEventListener("keydown", handleKeyDown);
+	  };
+	}, [closeModal]);
+  return createPortal(
+    <div className={css.backdrop} role="dialog" aria-modal="true" onClick={handleBackdropClick}>
+      <div className={css.modal}><NoteForm onClose={closeModal}/></div>
+    </div>,
+    document.body
+  );
+}
---
+++ lib/api.ts
@@ -0,0 +1,48 @@
+import axios from "axios";
+import type { FetchNoteResponse, NewNote, Note } from "@/types/note";
+
+
+
+const BASE_URL = "https://notehub-public.goit.study/api/notes"
+
+export async function fetchNotes(query: string, page: number): Promise<FetchNoteResponse> {
+    const res = await axios.get<FetchNoteResponse>(`${BASE_URL}`, {
+        params: {
+            page,
+            perPage: 12,
+            ...(query && { search: query }),
+        },
+        headers: {
+            Authorization: `Bearer ${process.env.NEXT_PUBLIC_NOTEHUB_TOKEN}`,
+            "Content-Type": "application/json",
+        }
+    })
+    return res.data
+}
+
+export async function createNote(userNote:NewNote): Promise<void> {
+    await axios.post<void>(`${BASE_URL}`, userNote, {
+        headers: {
+            Authorization: `Bearer ${process.env.NEXT_PUBLIC_NOTEHUB_TOKEN}`,
+            "Content-Type": "application/json",
+        }
+    })
+}
+
+export async function deleteNote(id:string):Promise<void> {
+    await axios.delete<void>(`${BASE_URL}/${id}`, {
+        headers: {
+            Authorization: `Bearer ${process.env.NEXT_PUBLIC_NOTEHUB_TOKEN}`,
+            "Content-Type": "application/json",
+        }
+    })
+    
+}
+
+export async function fetchNoteById (id:string): Promise<Note> {
+    const res = await axios.get<Note>(`${BASE_URL}/${id}`, {headers: {
+        Authorization: `Bearer ${process.env.NEXT_PUBLIC_NOTEHUB_TOKEN}`,
+            "Content-Type": "application/json",
+    }})
+    return res.data
+}
